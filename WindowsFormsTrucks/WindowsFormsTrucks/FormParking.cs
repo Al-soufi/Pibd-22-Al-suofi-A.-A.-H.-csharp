aed75bf1c8cb91c92c125ea9090b26167750a2ad
Al-suofi <alsuofi.x.stud@gamil.com>
2021-02-18 20:44:42 +0400

    Lab_4 is ready!

---
+++ WindowsFormsTrucks/WindowsFormsTrucks/FormParking.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsTrucks
+{
+    public partial class FormParking : Form
+    {
+        private readonly ParkingCollection parkingCollection;
+        public FormParking()
+        {
+            InitializeComponent();
+            parkingCollection = new ParkingCollection(pictureBoxParking.Width, pictureBoxParking.Height);
+            Draw();
+        }
+        private void ReloadLevels()
+        {
+            int index = listBoxParking.SelectedIndex;
+            listBoxParking.Items.Clear();
+            for (int i = 0; i < parkingCollection.Keys.Count; i++)
+            {
+                listBoxParking.Items.Add(parkingCollection.Keys[i]);
+            }
+            if (listBoxParking.Items.Count > 0 && (index == -1 || index >= listBoxParking.Items.Count))
+            {
+                listBoxParking.SelectedIndex = 0;
+            }
+            else if (listBoxParking.Items.Count > 0 && index > -1 && index < listBoxParking.Items.Count)
+            {
+                listBoxParking.SelectedIndex = index;
+            }
+        }
+        private void Draw()
+        {
+            if (listBoxParking.SelectedIndex > -1)
+            {
+                Bitmap bmp = new Bitmap(pictureBoxParking.Width,pictureBoxParking.Height);
+                Graphics gr = Graphics.FromImage(bmp);
+                parkingCollection[listBoxParking.SelectedItem.ToString()].Draw(gr);
+                pictureBoxParking.Image = bmp;
+            }
+        }
+        private void ParkingTruck_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                ColorDialog dialogDop = new ColorDialog();
+                if (dialogDop.ShowDialog() == DialogResult.OK)
+                {
+                    var truck = new Truck(100, 1000, dialog.Color, dialogDop.Color, true, true);
+                    if (parkingCollection[listBoxParking.SelectedItem.ToString()] + truck)
+                    {
+                        Draw();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Нет свободных мест", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                }
+            }
+        }
+        private void ParkingDumpTruck_Click(object sender, EventArgs e)
+        {
+            ColorDialog dialog = new ColorDialog();
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                ColorDialog dialogDop = new ColorDialog();
+                if (dialogDop.ShowDialog() == DialogResult.OK)
+                {
+                    var truck = new DumpTruck(100, 1000, dialog.Color, dialogDop.Color, true, true);
+                    if (parkingCollection[listBoxParking.SelectedItem.ToString()] + truck)
+                    {
+                        Draw();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Нет свободных мест", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+        private void Take_Click(object sender, EventArgs e)
+        {
+            if (listBoxParking.SelectedIndex > -1 && maskedTextBoxParking.Text != "")
+            {
+                var truck = parkingCollection[listBoxParking.SelectedItem.ToString()] - Convert.ToInt32(maskedTextBoxParking.Text);
+                if (truck != null)
+                {
+                    {
+                        FormTruck form = new FormTruck();
+                        form.SetTruck(truck);
+                        form.ShowDialog();
+                    }
+                    Draw();
+                }
+            }
+        }
+        private void Add_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(textBoxName.Text))
+            {
+                MessageBox.Show("Введите название парковки", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            parkingCollection.AddParking(textBoxName.Text);
+            ReloadLevels();
+        }
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            if (listBoxParking.SelectedIndex > -1)
+            {
+                if (MessageBox.Show($"Удалить парковку {listBoxParking.SelectedItem.ToString()}?", "Удаление", MessageBoxButtons.YesNo,MessageBoxIcon.Question) == DialogResult.Yes)
+                {
+                    parkingCollection.DelParking(textBoxName.Text);
+                    ReloadLevels();
+                }
+            }
+        }
+        private void listBoxParking_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Draw();
+        }
+    }
+}
---
+++ WindowsFormsTrucks/WindowsFormsTrucks/Parking.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace WindowsFormsTrucks
+{
+    public class Parking<T> where T : class, ITransport
+    {
+        private readonly List<T> Depot;
+        private readonly int MaxCount;
+        private readonly int pictureWidth;
+        private readonly int pictureHeight;
+        private readonly int place_Width = 210;
+        private readonly int place_Height = 85;
+        public Parking(int pic_Width, int pic_Height)
+        {
+            int width = pic_Width / place_Width;
+            int height = pic_Height / place_Height;
+            MaxCount = width * height;
+            pictureWidth = pic_Width;
+            pictureHeight = pic_Height;
+            Depot = new List<T>();
+        }
+        public static bool operator +(Parking<T> parking, T truck)
+        {
+            if (parking.Depot.Count >= parking.MaxCount)
+            {
+                return false;
+            }
+            parking.Depot.Add(truck);
+            return true;
+        }
+        public static T operator -(Parking<T> parking, int index)
+        {
+            if (index < -1 || index > parking.Depot.Count)
+            {
+                return null;
+            }
+            T truck = parking.Depot[index];
+            parking.Depot.RemoveAt(index);
+            return truck;
+        }
+        public void Draw(Graphics g)
+        {
+            DrawMarking(g);
+            for (int i = 0; i < Depot.Count; i++)
+            {
+                Depot[i].Position(i / 6 * place_Width, i % 6 * place_Height, pictureWidth, pictureHeight);
+                Depot[i].DrawTruck(g);
+            }
+        }
+        private void DrawMarking(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black, 3);
+            for (int i = 0; i < pictureWidth / place_Width; i++)
+            {
+                for (int j = 0; j < pictureHeight / place_Height + 1; ++j)
+                {
+                    g.DrawLine(pen, i * place_Width, j * place_Height, i * place_Width + 170  , j * place_Height);
+                }
+                g.DrawLine(pen, i * place_Width, 0, i * place_Width, (pictureHeight / place_Height) * place_Height);
+            }
+        }
+    }
+}
